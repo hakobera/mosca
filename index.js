9ee9e56a6edc349bd006841f5e6acc89d64b8677
Kazuyuki Honda <hakobera@gmail.com>
2014-06-30 15:20:18 +0900

    Export Client

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ module.exports.Server = require("./lib/server");
 module.exports.Authorizer = require("./lib/authorizer");
 module.exports.persistence = require("./lib/persistence");
 module.exports.Stats = require("./lib/stats");
+module.exports.Client = require('./lib/client');
